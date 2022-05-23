b795f8abd42b3092d2380aad8c6c815d4e9dd09d
Jordan Waldehz <jrwaldehzx@gmail.com>
2022-05-22 20:58:31 -0500

    db changes

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,18 +1,19 @@
-const parse = require("pg-connection-string");
+const parse = require('pg-connection-string').parse;
 const config = parse(process.env.DATABASE_URL);
 
 module.exports = ({ env }) => ({
   connection: {
-    client: "postgres",
+    client: 'postgres',
     connection: {
-      host: env("DATABASE_HOST", "127.0.0.1"),
-      port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", "strapi"),
-      user: env("DATABASE_USERNAME", "Pokerxx"),
-      password: env("DATABASE_PASSWORD", "Pokerx_007"),
-      schema: env("DATABASE_SCHEMA", "public"), // Not required
-      ssl: env("DATABASE_SSL", false)
+      host: config.host,
+      port: config.port,
+      database: config.database,
+      user: config.user,
+      password: config.password,
+      ssl: {
+        rejectUnauthorized: false
+      },
     },
-    debug: false
-  }
+    debug: false,
+  },
 });
