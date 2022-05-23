809f990a22c80c2ca8ae25dbfb7416f60f547ff8
Jordan Waldehz <jrwaldehzx@gmail.com>
2022-05-22 20:39:19 -0500

    updated ssl

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,20 +1,24 @@
-const parse = require('pg-connection-string')
-const config = parse(process.env.DATABASE_URL)
-
+const parse = require("pg-connection-string");
+const config = parse(process.env.DATABASE_URL);
 
 module.exports = ({ env }) => ({
+  connection: {
+    client: "postgres",
     connection: {
-        client: 'postgres',
-        connection: {
-            host: config.host,
-            port: config.port,
-            database: config.database,
-            user: config.user,
-            password: config.password,
-            ssl: {
-                rejectUnauthorized: false
-            },
-        },
-        debug: false,
+      host: config.host,
+      port: config.port,
+      database: config.database,
+      user: config.user,
+      password: config.password,
+      ssl: {
+        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
+      },     
     },
-})
+    // add this line
+    options: {
+      ssl: env.bool('DATABASE_SSL', false),
+    },
+
+    debug: false
+  }
+});
