6d4ab149ba89de54f08e4b982228555ee2e63fa5
Jordan Waldehz <jrwaldehzx@gmail.com>
2022-05-22 20:50:24 -0500

    updated ssl2

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -5,20 +5,14 @@ module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: config.host,
-      port: config.port,
-      database: config.database,
-      user: config.user,
-      password: config.password,
-      ssl: {
-        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
-      },     
+      host: env("DATABASE_HOST", "127.0.0.1"),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME", "strapi"),
+      user: env("DATABASE_USERNAME", "Pokerxx"),
+      password: env("DATABASE_PASSWORD", "Pokerx_007"),
+      schema: env("DATABASE_SCHEMA", "public"), // Not required
+      ssl: env("DATABASE_SSL", false)
     },
-    // add this line
-    options: {
-      ssl: env.bool('DATABASE_SSL', false),
-    },
-
     debug: false
   }
 });
